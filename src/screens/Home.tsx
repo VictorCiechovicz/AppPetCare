50656dff1d06c37560af1cfa1f7cd612e0490a28
victor ciechovicz <victor09ciechovicz@outlook.com>
2022-09-03 16:04:58 -0300

    up

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,49 @@
-import { HStack, IconButton, VStack, useTheme } from 'native-base'
-import { SignOut } from 'phosphor-react-native'
+import { useState } from 'react'
+
+import {
+  HStack,
+  IconButton,
+  VStack,
+  useTheme,
+  Text,
+  Heading,
+  FlatList,
+  Circle
+} from 'native-base'
+import { SignOut, ChatTeardropText } from 'phosphor-react-native'
 
 import Logo from '../../assets/logo_secondary.svg'
 
+import { Filter } from '../components/Filter'
+import { Button } from '../components/Button'
+import { Order, OrderProps } from '../components/Order'
+
 export function Home() {
+  const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>(
+    'open'
+  )
+
+  const [orders, serOrders] = useState<OrderProps[]>([
+    {
+      id: '123',
+      patrimony: '25425',
+      when: '03/09/2022 as 14:00',
+      status: 'open'
+    },
+    {
+      id: '124',
+      patrimony: '25425',
+      when: '03/09/2022 as 14:00',
+      status: 'open'
+    },
+    {
+      id: '125',
+      patrimony: '25425',
+      when: '03/09/2022 as 14:00',
+      status: 'open'
+    }
+  ])
+
   const { colors } = useTheme()
 
   return (
@@ -21,6 +61,53 @@ export function Home() {
 
         <IconButton icon={<SignOut size={26} color={colors.gray[300]} />} />
       </HStack>
+
+      <VStack flex={1} px={6}>
+        <HStack
+          w="full"
+          mt={8}
+          mb={4}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Heading color="gray.100">Meus chamados</Heading>
+          <Text color="gray.200">3</Text>
+        </HStack>
+        <HStack space={3} mb={8}>
+          <Filter
+            type="closed"
+            title="em andamento"
+            onPress={() => setStatusSelected('open')}
+            isActive={statusSelected === 'open'}
+          />
+          <Filter
+            type="open"
+            title="finalizados"
+            onPress={() => setStatusSelected('closed')}
+            isActive={statusSelected === 'closed'}
+          />
+        </HStack>
+
+        <FlatList
+          data={orders}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <Order data={item} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 100 }}
+          ListEmptyComponent={() => (
+            <Circle>
+              <ChatTeardropText color={colors.gray[300]} size={40} />
+              <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
+                Voce ainda nao possui {'\n'}
+                solicitacoes{' '}
+                {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
+              </Text>
+            </Circle>
+          )}
+        />
+
+        <Button title="Nova solicitacao" />
+      </VStack>
     </VStack>
   )
 }
