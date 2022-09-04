1f562d297689c98c132d13e661ff6592ca675da9
victor ciechovicz <victor09ciechovicz@outlook.com>
2022-09-04 19:40:10 -0300

    up

---
+++ src/screens/Details.tsx
@@ -0,0 +1,15 @@
+
+import { VStack } from 'native-base';
+
+import { Header } from '../components/Header';
+
+export function Details() {
+  return (
+    <VStack flex={1} bg="yellow.100">
+<Header 
+title="Solicitacao"
+
+/>
+    </VStack>
+  );
+}
