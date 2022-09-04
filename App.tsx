31f8ebdf9417e5232e8ca4a22580a1861181149c
victor ciechovicz <victor09ciechovicz@outlook.com>
2022-09-04 19:35:04 -0300

    up

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import { THEME } from './src/styles/theme'
 import { SignIn } from './src/screens/SignIn'
 import { Loading } from './src/components/Loading'
 
-import { Home } from './src/screens/Home'
+import { Register } from './src/screens/Register'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -21,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Home /> : <Loading />}
+      {fontsLoaded ? <Register /> : <Loading />}
     </NativeBaseProvider>
   )
 }
