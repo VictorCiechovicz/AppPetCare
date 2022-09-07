1976d2e064ba63e01f3eb992d40f4c286da86c7c
victor ciechovicz <victor09ciechovicz@outlook.com>
2022-09-07 01:16:26 -0300

    up

--- App.tsx
+++ App.tsx
@@ -21,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   )
 }
